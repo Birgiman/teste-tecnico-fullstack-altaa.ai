75c190e06c8479e69ed04ba8ba0253b5e9daa8b8
Birgiman <eduardo.birgiman@outlook.com>
2025-11-07 23:52:54 -0300

    🔧 Adiciona configuração Docker e Docker Compose para backend e banco de dados PostgreSQL

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import Fastify from "fastify";
+
+const app = Fastify();
+
+const port = parseInt(process.env.PORT || '4000');
+const host = process.env.HOST || '0.0.0.0';
+
+app.get(
+  '/',
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async (request, reply) => {
+    return { message: 'Backend rodando com Fastify!'}
+  }
+)
+
+app.listen({ port, host }, (err: Error | null, address: string) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`🚀 - Servidor rodando em ${address}`);
+})
