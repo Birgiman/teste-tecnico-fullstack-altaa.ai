4ded389a3baa2cccf1cc48305eabdf870ec48df0
Birgiman <eduardo.birgiman@outlook.com>
2025-11-08 13:42:59 -0300

    ➕ Chore(backend): finaliza configuração Fastify e aprimora logging com pino-pretty
    - adiciona dotenv para variáveis ambiente e valida obrigatoriedade

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,52 @@
+import fastifyCookie from '@fastify/cookie';
+import fastifyCors from '@fastify/cors';
+import "dotenv/config";
 import Fastify from "fastify";
 
-const app = Fastify();
+function getEnvVars(name: string): string {
+  const value = process.env[name];
+  if (!value) {
+    throw new Error(`🔓 - Variável de ambiente ${name} não encontrada`);
+  }
+  return value;
+}
 
-const port = parseInt(process.env.PORT || '4000');
-const host = process.env.HOST || '0.0.0.0';
+const port = parseInt(getEnvVars('PORT'));
+const host = getEnvVars('HOST');
+const frontEndUrl = getEnvVars('FRONT_END_URL');
+const cookieSecret = getEnvVars('COOKIE_SECRET');
+
+const app = Fastify({ logger: true });
+
+await app.register(fastifyCors, {
+  origin: frontEndUrl,
+  credentials: true,
+})
+
+await app.register(fastifyCookie, {
+  secret: cookieSecret,
+  parseOptions: {}
+})
+
+app.setErrorHandler((error, request, reply) => {
+  app.log.error(error);
+  reply.status(500).send({ error: error.message || 'Internal Server Error'})
+})
 
 app.get(
   '/',
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async (request, reply) => {
-    return { message: 'Backend rodando com Fastify!!!!!!'}
+    return { message: 'Backend rodando com Fastify!'}
   }
 )
 
-app.listen({ port, host }, (err: Error | null, address: string) => {
+app.listen({ port, host }, (err: Error | null) => {
   if (err) {
-    console.error(err);
+    app.log.error(`🔴 - Erro ao iniciar o servidor: ${err.message}`);
     process.exit(1);
   }
-  console.log(`🚀 - Servidor rodando em ${address}`);
+  console.log(`🚀 - Servidor rodando em http://${host}:${port}`);
+  console.log(`🚧 - Prisma Studio disponível em http://${host}:5555`);
+  console.log("🔛 - Ouvindo Front-End em", frontEndUrl);
 })
